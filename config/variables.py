dafd596bb0102dad02cb97544ad5b9526252ffd2
NickOsipov <nick.osipov.91@gmail.com>
2026-07-08 21:41:21 +0300

    feat: implement CI/CD pipeline, add Docker support, and configure environment variables

--- config/variables.py
+++ config/variables.py
@@ -6,6 +6,10 @@ including database connection details, model path, and class labels.
 
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
@@ -15,3 +19,12 @@ DB_NAME = os.getenv("DB_NAME")
 MODEL_PATH = os.path.join("models", "model.joblib")
 IRIS_CLASSES = ["setosa", "versicolor", "virginica"]
 
+REGISTRY_USER = "nickosipov"
+IMAGE_NAME = "flask-app"
+CONTAINER_NAME = "flask-app"
+
+REMOTE_HOST = os.getenv("REMOTE_HOST")
+REMOTE_PATH = os.getenv("REMOTE_PATH")
+
+SSH_KEY_PATH = os.getenv("SSH_KEY_PATH")
+SSH_USER = os.getenv("SSH_USER", "ubuntu")  # Default to 'ubuntu' if not set
--- tasks.py
+++ tasks.py
@@ -1,23 +1,45 @@
 from invoke import task
 
+from config.variables import (
+    REGISTRY_USER,
+    IMAGE_NAME,
+    CONTAINER_NAME,
+    REMOTE_HOST,
+    REMOTE_PATH,
+    SSH_KEY_PATH,
+    SSH_USER,
+)
+
 
 @task
 def build(ctx):
     """Build the Docker image."""
-    cmd = "docker build -t flask-app:latest ."
+    cmd = f"docker build -t {IMAGE_NAME}:latest ."
     ctx.run(cmd, pty=True)
+
+
+@task
+def push(ctx):
+    """Push the Docker image to a registry."""
+    cmd = f"docker tag {IMAGE_NAME}:latest {REGISTRY_USER}/{IMAGE_NAME}:latest"
+    ctx.run(cmd, pty=True)
+    cmd = f"docker push {REGISTRY_USER}/{IMAGE_NAME}:latest"
+    ctx.run(cmd, pty=True)
+
 
 @task
 def stop(ctx):
     """Stop the Docker container."""
-    cmd = "docker stop flask-app && docker rm flask-app"
+    cmd = f"docker stop {CONTAINER_NAME} && docker rm {CONTAINER_NAME}"
     ctx.run(cmd, pty=True)
+
 
 @task
 def run(ctx):
     """Run the Docker container."""
-    cmd = "docker run -p 5000:5000 --name flask-app flask-app"
+    cmd = f"docker run -p 5000:5000 --name {CONTAINER_NAME} {IMAGE_NAME}"
     ctx.run(cmd, pty=True)
+
 
 @task
 def up(ctx):
@@ -27,10 +49,29 @@ def up(ctx):
     cmd = "docker compose up -d"
     ctx.run(cmd, pty=True)
 
+
 @task
 def down(ctx):
     """
     Stop the Docker containers defined in the docker-compose file.
     """
     cmd = "docker compose down"
+    ctx.run(cmd, pty=True)
+
+
+@task
+def scp(ctx):
+    """
+    Copy files to a remote host using SCP.
+    """
+
+    cmd = f"scp -i {SSH_KEY_PATH} -r docker-compose.yml migrations {SSH_USER}@{REMOTE_HOST}:{REMOTE_PATH}"
+    ctx.run(cmd, pty=True)
+
+@task
+def deploy(ctx):
+    """
+    Deploy the application to a remote host.
+    """
+    cmd = f"ssh -i {SSH_KEY_PATH} {SSH_USER}@{REMOTE_HOST} 'cd {REMOTE_PATH} && docker compose down && docker compose up -d'"
     ctx.run(cmd, pty=True)
